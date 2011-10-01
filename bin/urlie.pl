72c3d85b71f832cf8eca7ccb4c4eb1c15b767ad1
Salve J. Nilsen <sjn@cpan.org>
2011-10-01 14:41:48 +0200

    Move to Bot::BasicBot infrastructure

--- bin/urlie.pl
+++ bin/urlie.pl
@@ -1,95 +1,56 @@
 #!/usr/bin/env perl
 
-package POE::Component::IRC::Plugin::WWW::GetPageTitle::ToChannel;
-use base 'POE::Component::IRC::Plugin::WWW::GetPageTitle';
-use NEXT;
-
-# Override the "public" command so it always sends a notice to the channel
-sub S_public {
-    my ( $self, $irc ) = splice @_, 0, 2;
-    my $who     = ${ $_[1] }->[0];    # We reply to channel, not person
-    my $channel = ${ $_[1] }->[0];
-    my $message = ${ $_[2] };
-    return $self->_parse_input( $irc, $who, $channel, $message, 'notice' );
-}
-
-package main;
-
 use 5.010_001;
 use strict;
 use warnings;
+
 use Config::File qw(read_config_file);
-use POE qw( Component::IRC
-  Component::IRC::Plugin::WWW::GetPageTitle
-  Component::IRC::Plugin::Connector );
 use Data::Dumper::Concise;
+use Bot::BasicBot::Pluggable;
 
 my $configuration_file = $ARGV[0] // "$ENV{HOME}/.urlierc";
 my $config             = Config::File::read_config_file($configuration_file);
 
-my $DEBUG = $config->{DEBUG} || 0;
+my $DEBUG = $ENV{URLIE_DEBUG} || $config->{DEBUG} || 0;
 warn Dumper($config) if $DEBUG;
 
 our $NICK     = $config->{Server}{Nick}     // 'urlie_' . $$ % 1000;
-our $IRCNAME  = $config->{Server}{IRCName}  // 'urlie is a bot';
+our $IRCNAME  = $config->{Server}{IRCName}  // q(urlie is sjn's bot);
 our $CHAN     = $config->{Server}{Chan}     // '#testbot_' . $$ % 1000;
 our $SERVER   = $config->{Proxy}{Host}      // 'irc.freenode.net';
 our $PORT     = $config->{Proxy}{Port}      // 6667;
 our $PASSWORD = $config->{Proxy}{Password}  // '';
 
 # Set process name, so we can recognize it with ps(1)
-$0 = 'urlie/' . $NICK . $CHAN . '@' . $SERVER . ':' . $PORT;
+$0 = $0 . '/' . $NICK . $CHAN . '@' . $SERVER . ':' . $PORT;
 
-my $irc = POE::Component::IRC->spawn(
-    nick         => $NICK,
-    ircname      => $IRCNAME,
+my $bot = Bot::BasicBot::Pluggable->new(
+    channels     => [ $CHAN ],
     server       => $SERVER,
     port         => $PORT,
+    nick         => $NICK,
+    altnicks     => [ "${NICK}_", "${NICK}__" ],
+    username     => "sjn_bot",
+    name         => $IRCNAME,
     password     => $PASSWORD,
     plugin_debug => $DEBUG,
 );
 
-POE::Session->create(
-    package_states => [ main => [qw(_start irc_001)], ],
-    heap           => { irc  => $irc },
-);
+my $auth_module    = $bot->load("Auth");
+$auth_module->set( password_admin => $PASSWORD );
+my $chanop_module  = $bot->load("ChanOp");
+my $dns_module     = $bot->load("DNS");
+my $infobot_module = $bot->load("Infobot");
+my $join_module    = $bot->load("Join");
+my $karma_module   = $bot->load("Karma");
+my $loader_module  = $bot->load("Loader");
+my $seen_module    = $bot->load("Seen");
+my $title_module   = $bot->load("Title");
+$title_module->set( user_be_rude => 1 );
+my $vars_module    = $bot->load("Vars");
 
-$poe_kernel->run;
 
-sub _start {
-    my ( $kernel, $heap ) = @_[ KERNEL, HEAP ];
-    my $irc = $heap->{irc};
 
-    $irc->yield( register => 'all' );
+say "Starting $NICK on $CHAN\@$SERVER";
 
-    $irc->plugin_add(
-        'Connector' => POE::Component::IRC::Plugin::Connector->new() );
-
-    $irc->plugin_add(
-        'get_page_title' =>
-          POE::Component::IRC::Plugin::WWW::GetPageTitle::ToChannel->new(
-            auto             => 1,
-            addressed        => 0,
-            find_uris        => 1,
-            max_uris         => 2,
-            banned           => [],
-            eat              => 1,
-            debug            => $DEBUG,
-            listen_for_input => [qw(public privmsg notice)],
-            response_event   => 'irc_get_page_title',
-            response_types   => {
-                public  => 'public',
-                privmsg => 'privmsg',
-                notice  => 'notice',
-            },
-            trigger => qr/^/i,
-          )
-    );
-
-    $irc->yield( connect => {} );
-}
-
-sub irc_001 {
-    $_[KERNEL]->post( $_[SENDER] => join => $CHAN );
-}
-
+$bot->run;
